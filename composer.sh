510c2d585324a843aec202ef82bc8ddef847016e
血衫非弧 <admin@kelu.org>
2018-09-09 16:13:54 +0800

    [update] update php image

---
+++ composer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+MYWORKSPACE="/Users/kelu/workspace/wechat.kelu.org"
+
+docker run -it --rm \
+  -v $MYWORKSPACE:/app \
+  --entrypoint=/bin/bash \
+  composer:1.4.2 
+
+docker run -it --rm \
+  -v $MYWORKSPACE:/app \
+  composer:1.4.2 dump-autoload
+
+docker run -it --rm \
+  -v $MYWORKSPACE:/app \
+  composer:1.4.2 install --ignore-platform-reqs --no-scripts

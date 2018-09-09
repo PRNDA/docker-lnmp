ca1589ce96d299e32f9e832393f6902d5bc3d1c5
血衫非弧 <admin@kelu.org>
2018-09-09 10:07:19 +0800

    [new] bugfix and add composer

--- boot.sh
+++ boot.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
+MYWORKSPACE="/Users/kelu/workspace/wechat.kelu.org"
+# MYWORKSPACE=$PWD/php/www
+
 docker run -d -p 8000:80 \
   --name nginx \
+  --restart=always \
+  -v $MYWORKSPACE:/var/www/html \
   -v $PWD/openresty:/etc/nginx/conf.d \
-  -v $PWD/sock:/sock:rw \
-  -v $PWD/log:/log:rw \
+  -v $PWD/sock:/sock \
+  -v $PWD/log:/log \
+  -v $PWD/bin/php:/usr/local/bin/php \
   openresty/openresty:alpine
 
 
 docker run --name php -d \
-    -v $PWD/php/www:/var/www/html:rw \
-    -v $PWD/php/php-fpm.d:/usr/local/etc/php-fpm.d:ro \
-    -v $PWD/php/conf/php-fpm.conf:/usr/local/etc/php-fpm.conf:ro \
-    -v $PWD/sock:/sock \
-    -v $PWD/log:/log:rw \
-    php:7.1-fpm
+  --restart=always \
+  -v $MYWORKSPACE:/var/www/html \
+  -v $PWD/php/php-fpm.d:/usr/local/etc/php-fpm.d:ro \
+  -v $PWD/php/conf/php-fpm.conf:/usr/local/etc/php-fpm.conf:ro \
+  -v $PWD/sock:/sock \
+  -v $PWD/log:/log \
+  php:7.1-fpm
+
+#docker run --rm \
+#  -v $MYWORKSAPCE:/app \
+#  composer:1.4.2 install --ignore-platform-reqs --no-scripts
--- test.sh
+++ test.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+MYWORKSPACE="/Users/kelu/workspace/wechat.kelu.org"
+
+
+#docker run -it --rm \
+#    -v $PWD/php/www:/var/www/html:rw \
+#    -v $PWD/php/php-fpm.d:/usr/local/etc/php-fpm.d:ro \
+#    -v $PWD/sock:/sock \
+#    -v $PWD/log:/log:rw \
+#    --entrypoint=/bin/bash \
+#    php:7.1-fpm
+
 docker run -it --rm \
-    -v $PWD/php/www:/var/www/html:rw \
-    -v $PWD/php/php-fpm.d:/usr/local/etc/php-fpm.d:ro \
-    -v $PWD/sock:/sock \
-    -v $PWD/log:/log:rw \
-    --entrypoint=/bin/bash \
-    php:7.1-fpm
+  -v $MYWORKSPACE:/app \
+  composer:1.4.2 install --ignore-platform-reqs --no-scripts
+
+#docker run -it --rm \
+#  -v $MYWORKSPACE:/app \
+#  --entrypoint=/bin/bash \
+#  composer:1.4.2
